46c1ac9104b2170e3314048a8af14aa902d0a0ce
sunayana <59723931+sunayana77@users.noreply.github.com>
2025-06-06 15:18:30 +0545

    Update credentials.py
    changes in credentials

--- credentials.py
+++ credentials.py
@@ -1,8 +1,8 @@
 import os
 
 class Credentials:
-    EMAIL = os.getenv("USER_EMAIL", "poudelsunayana@gmail.com")  # Fetch from environment variable
-    PASSWORD = os.getenv("USER_PASSWORD", "Susuna123@")  # Fetch from environment variable
+    EMAIL = os.getenv("USER_EMAIL", "your_email")  # Fetch from environment variable
+    PASSWORD = os.getenv("USER_PASSWORD", "your_password")  # Fetch from environment variable
 
     @staticmethod
     def get_credentials():
